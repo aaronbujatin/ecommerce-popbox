ba62dd35a0c27aeda79453802e02770abc64fd44
Aaron Bujatin <bujatinaaron@gmail.com>
2026-01-28 17:57:17 +0800

    update auth svc

--- auth-svc/src/main/java/org/xyz/authsvc/AuthsvcApplication.java
+++ auth-svc/src/main/java/org/xyz/authsvc/AuthsvcApplication.java
@@ -1,7 +1,10 @@
 package org.xyz.authsvc;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AuthsvcApplication {
@@ -9,5 +12,4 @@ public class AuthsvcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthsvcApplication.class, args);
 	}
-
 }
